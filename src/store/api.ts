6a0eaa929d9c5d9b4f316c6d18d14004b24a3c4b
Avinash0526 <avinashm0526@gamil.com>
2025-09-30 06:14:24 +0530

    Have updated api.tsx

--- src/store/api.ts
+++ src/store/api.ts
@@ -2,29 +2,31 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "https://jsonplaceholder.typicode.com/" }), // change later
+  baseQuery: fetchBaseQuery({ baseUrl: "https://jsonplaceholder.typicode.com/" }),
   tagTypes: ["CRUD"],
   endpoints: (builder) => ({
     getItems: builder.query<any[], string>({
       query: (endpoint) => endpoint,
       providesTags: ["CRUD"],
     }),
-    createItem: builder.mutation<any, { endpoint: string; body: any }>({
-      query: ({ endpoint, body }) => ({
+    createItem: builder.mutation<any, { endpoint: string; method?: string; body: any }>({
+      query: ({ endpoint, method, body }) => ({
         url: endpoint,
-        method: "POST",
+        method: method || "POST",
         body,
       }),
       invalidatesTags: ["CRUD"],
     }),
-    updateItem: builder.mutation<any, { endpoint: string; id: string; body: any }>({
-      query: ({ endpoint, id, body }) => ({
-        url: `${endpoint}/${id}`,
-        method: "PUT",
-        body,
-      }),
-      invalidatesTags: ["CRUD"],
-    }),
+    updateItem: builder.mutation<any, { endpoint: string; id: string; method?: string; body: any }>(
+      {
+        query: ({ endpoint, id, method, body }) => ({
+          url: `${endpoint}/${id}`,
+          method: method || "PUT",
+          body,
+        }),
+        invalidatesTags: ["CRUD"],
+      },
+    ),
     deleteItem: builder.mutation<any, { endpoint: string; id: string }>({
       query: ({ endpoint, id }) => ({
         url: `${endpoint}/${id}`,
